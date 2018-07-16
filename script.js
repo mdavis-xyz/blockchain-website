c41d878a170bb7976bdc4ae7c2106df1be0e0969
Matthew Davis <matthew@mdavis.xyz>
2018-07-16 22:24:45 +1000

    left to right animations, vert blocky

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
-  /* put your JavaScript code here */
-  alert("ready");
+   var $sliderL = document.getElementById('slider-l');
+   var $sliderR = document.getElementById('slider-r');
+   var $toggle = document.getElementById('toggle');
+
+   $toggle.addEventListener('click', function() {
+       var isOpen = $sliderL.classList.contains('slide-in');
+
+       $sliderL.setAttribute('class', isOpen ? 'slider slide-out' : 'slider slide-in');
+       $sliderR.setAttribute('class', isOpen ? 'slider slide-in' : 'slider slide-out');
+   });
 });
