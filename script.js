b698c27b32a82fd1ad7299a64b4a82f2b3c10f1e
Matthew Davis <matthew@mdavis.xyz>
2018-07-18 21:14:44 +1000

    bottom margin fixed, auto scroll to top on new

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function slide(oldElID,newElID,direction){
    var newEl = document.getElementById(newElID);
    var oldEl = document.getElementById(oldElID);
    console.log(`sliding ${oldElID} to ${direction} to make room for ${newElID} `);
+   newEl.scrollTop = 0;
    newEl.setAttribute('class', 'slide-in-' + direction);
    oldEl.setAttribute('class', 'slide-out-' + direction);
 }
