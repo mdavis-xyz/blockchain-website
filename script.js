4f4bba8662b54437f472b63dd2110cfafa4f099b
Matthew Davis <matthew@mdavis.xyz>
2018-07-18 22:41:54 +1000

    back button works

--- script.js
+++ script.js
@@ -1,17 +1,65 @@
+
+var backButton = null;
+var navHistory = [];
+var navDirections = [];
+var currentElID = null; // won't be used until we navigate anyway
+
 $(document).ready(function() {
    var $sliderL = document.getElementById('slider-l');
    var $sliderR = document.getElementById('slider-r');
    var $toggle = document.getElementById('toggle');
 
-
+   backButton = document.getElementById('back-button');
 
 });
 
+
+
 function slide(oldElID,newElID,direction){
+   slideNoHist(oldElID,newElID,direction);
+   if(navHistory.length == 0) {
+      document.getElementById("back-button").setAttribute('class', 'left-arrow slide-in-button'); // not hidden
+   }
+   navHistory.push(oldElID);
+   navDirections.push(direction);
+}
+
+function slideNoHist(oldElID,newElID,direction){
    var newEl = document.getElementById(newElID);
    var oldEl = document.getElementById(oldElID);
    console.log(`sliding ${oldElID} to ${direction} to make room for ${newElID} `);
    newEl.scrollTop = 0;
    newEl.setAttribute('class', 'slide-in-' + direction);
    oldEl.setAttribute('class', 'slide-out-' + direction);
+   currentElID = newElID;
+}
+
+function reverseDirection(prevDir){
+   var newDir;
+   switch(prevDir.toLowerCase()) {
+       case 'top':
+           newDir = 'bottom';
+           break;
+       case 'bottom':
+           newDir = 'top';
+           break;
+       case 'left':
+           newDir = 'right';
+           break;
+       case 'right':
+           newDir = 'left';
+           break;
+   }
+   console.log(`reverse of ${prevDir} is ${newDir}`);
+   return(newDir);
+}
+
+function back(){
+   var newElID = navHistory.pop();
+   var direction = reverseDirection(navDirections.pop());
+   console.log(`Sliding back from ${currentElID} to ${newElID}`);
+   slideNoHist(currentElID,newElID,direction);
+   if(navHistory.length == 0) {
+      backButton.setAttribute('class', 'left-arrow slide-out-button');
+   }
 }
